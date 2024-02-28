edc77bee59178fc5df48b502f0f3954b11010576
bhava <sreerastu07@gmail.com>
2024-02-28 12:20:22 +0530

    branch1 commit

---
+++ src/main/java/com/Project/Hope/Entity/Clinic.java
@@ -0,0 +1,29 @@
+package com.Project.Hope.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+@Table (name = "CLINIC_TBL")
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Clinic {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected int clinicId;
+    protected String Name;
+    protected String Location;
+    protected String contactNumber;
+    protected String emailAddress;
+    protected String password;
+    protected int clinicDoctorId;
+    protected String clinicHead;
+    protected String clinicHeadNumber;
+    protected String clinicWebsite;
+    protected String clinicCertificate;
+
+}
