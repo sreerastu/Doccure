ab223989913128f5531c9fc1918121a0c202cfc7
bhava <sreerastu07@gmail.com>
2024-02-28 11:38:43 +0530

    initial commit

---
+++ src/main/java/com/Project/Hope/HopeApplication.java
@@ -0,0 +1,14 @@
+package com.Project.Hope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopeApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Project/Hope/HopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Project.Hope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
